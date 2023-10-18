f3c23f66374293d2665bbb59a19b95b767b8bdb9
Sangay Jamtsho <12200017.gcit@rub.edu.bt>
2023-10-18 23:18:41 +0600

    + archive model

---
+++ app/Models/Archive.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Archive extends Model
+{
+    protected $table = 'archive';
+
+    protected $fillable = ['fileURL', 'archivedDate', 'archivedBy'];
+
+    // Add any additional properties, relationships, or methods as needed
+}
